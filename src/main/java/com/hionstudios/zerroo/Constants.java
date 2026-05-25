e76bf888d547b59115d762ea2250d0e3661d2cc3
Alagendran <oxyian@outlook.com>
2026-05-25 15:18:06 +0000

    fix4

--- src/main/java/com/hionstudios/zerroo/Constants.java
+++ src/main/java/com/hionstudios/zerroo/Constants.java
@@ -3,7 +3,7 @@ package com.hionstudios.zerroo;
 import com.hionstudios.zerroo.model.IncomeWalletTransactionType;
 
 public class Constants {
-    public static final String FY = "24-25";
+    public static final String FY = "26-27";
     public static final String SHIPPING_SAC = "996812";
     public static final double MIN_PAYOUT = 100;
     public static final long ACTIVATION_PV = 5;
